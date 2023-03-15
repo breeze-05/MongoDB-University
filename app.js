a850e287f7560fc3f8f4cc5bd074e119e784491d
breeze-05 <124463274+breeze-05@users.noreply.github.com>
2023-03-15 01:33:25 +0000

    Checkpoint 1

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const uri = require('./atlas_uri');
 const consola = require('consola');
 const { connect } = require('http2');
 const { errorMonitor } = require('stream');
+const { waitForDebugger } = require('inspector');
+const prompt = require("prompt-sync")({ sigint: true });
 
-console.log(uri);
+// console.log(uri);
 
 const client = new MongoClient(uri)
 const dbname = "Playground"
+const collectionName = "RobloxStats"
+
+const collection = client.db(dbname).collection(collectionName);
+
 
 const connectToDatabase = async () => {
   try {
@@ -18,13 +24,25 @@ const connectToDatabase = async () => {
   }
 };
 
+const robloxStats = {
+  "robloxId": 3152840,
+  "robux": 3750,
+  "visits": 1000000000,
+  "lastUpdated": new Date()
+ }
+
+
 const main = async () => {
   try {
     await connectToDatabase();
+    let result = await collection.insertOne(robloxStats);
+    console.log(`Successfully inserted ${result.insertedCount}`);
   } catch (err) {
     console.error(`Error connecting to the database: ${err}`);
   } finally {
     await client.close();
   }
 };
+
+// connect to database
  main();
